423593ae5281ff2680501eb11bde9cfbb072f6e1
Dmitry Ovchinnikov <dmitry@aworld.games>
2022-08-16 12:55:58 +0500

    Fix type error

--- pages/list.tsx
+++ pages/list.tsx
@@ -3,17 +3,17 @@ import { useState } from 'react'
 import BasicLayout from 'components/layout/Basic'
 import {
   List,
-  Tag,
+  TagType,
 } from 'components/pages/List'
 
-const TAGS_ARRAY: Tag[] = [
+const TAGS_ARRAY: TagType[] = [
   { title: 'World Of Tanks', id: 0 },
   { title: 'World Of Warplanes', id: 1 },
   { title: 'World Of Warships', id: 2 },
 ]
 
 const ListPage = () => {
-  const [selectedTag, setSelectedTag] = useState<Tag>(TAGS_ARRAY[1])
+  const [selectedTag, setSelectedTag] = useState<TagType>(TAGS_ARRAY[1])
 
   const handleSelectTag = (id: number) => {
     const tag = TAGS_ARRAY.find((tag) => tag.id === id)
